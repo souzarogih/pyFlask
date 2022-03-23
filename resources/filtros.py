524b0a4e0dc786977865151199c85f033a804ec3
HigorSouza <higor.andrade@hotmail.com>
2022-03-22 23:39:28 -0300

    Incluindo filtros para consulta avançada

---
+++ resources/filtros.py
@@ -0,0 +1,36 @@
+
+def normalize_path_params(cidade=None,
+                           estelas_min = 0,
+                           estelas_max = 5,
+                           diaria_min = 0,
+                           diaria_max = 10000,
+                           limit = 50,
+                           offset = 0, **dados):
+    if cidade:
+        return {
+            'estelas_min' : estelas_min,
+            'estelas_max' : estelas_max,
+            'diaria_min' : diaria_min,
+            'diaria_max' : diaria_max,
+            'cidade' : cidade,
+            'limit' : limit,
+            'offset' : offset}
+    return {
+        'estelas_min': estelas_min,
+        'estelas_max': estelas_max,
+        'diaria_min': diaria_min,
+        'diaria_max': diaria_max,
+        'limit': limit,
+        'offset': offset}
+
+
+consulta_sem_cidade = "SELECT * FROM hoteis \
+            WHERE (estrelas >= ? and estrelas <= ?) \
+            and (diaria >= ? and diaria <= ?) \
+            LIMIT ? OFFSET ?"
+
+
+consulta_com_cidade = "SELECT * FROM hoteis \
+            WHERE (estrelas >= ? and estrelas <= ?) \
+            and (diaria >= ? and diaria <= ?) \
+            and cidade = ? LIMIT ? OFFSET ?"
--- resources/hotel.py
+++ resources/hotel.py
@@ -1,11 +1,49 @@
+import sqlite3
+
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
 from flask_jwt_extended import jwt_required
+from resources.filtros import normalize_path_params, consulta_com_cidade, consulta_sem_cidade
+
+
+path_params = reqparse.RequestParser()
+path_params.add_argument('cidade', type=str)
+path_params.add_argument('estelas_min', type=float)
+path_params.add_argument('estelas_max', type=float)
+path_params.add_argument('diaria_min', type=float)
+path_params.add_argument('diaria_max', type=float)
+path_params.add_argument('limit', type=float)
+path_params.add_argument('offset', type=float)
+
 
 class Hoteis(Resource):
     def get(self):
-        return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]}
-        # return {'hoteis': 'meus hoteis'} #Listando apenas um dicionario
+        connection = sqlite3.connect('banco.db')
+        cursor = connection.cursor()
+
+        dados = path_params.parse_args()
+        dados_validos = {chave:dados[chave] for chave in dados if dados[chave] is not None}
+        parametros = normalize_path_params(**dados_validos)
+
+        if not parametros.get('cidade'):
+            tupla = tuple([parametros[chave] for chave in parametros])
+            resultado = cursor.execute(consulta_sem_cidade, tupla)
+        else:
+            tupla = tuple([parametros[chave] for chave in parametros])
+            resultado = cursor.execute(consulta_com_cidade, tupla)
+
+        hoteis = []
+        for linha in resultado:
+            hoteis.append({
+                'hotel_id': linha[0],
+                'nome': linha[1],
+                'estrelas': linha[2],
+                'diaria': linha[3],
+                'cidade': linha[4]
+            })
+
+        # return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]}
+        return {'hoteis': hoteis}
 
 
 class Hotel(Resource):
